a2bc279bba1984023b708cce84fee5dfcac8a4c5
dinali3 <gayaradinali96@gmail.com>
2022-08-20 01:06:31 +0530

    Add project files.

---
+++ Task2/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Task2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task2.Data
+{
+    public class ContactsAPIDbContext: DbContext
+    {
+        
+        public ContactsAPIDbContext(DbContextOptions options):base(options)
+            {
+
+            }
+        public DbSet<Contact> Contacts
+        {
+            get; set; 
+        }
+
+    }
+}
---
+++ Task2/Models/UpdateContactRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2.Models
+{
+    public class UpdateContactRequest
+    {
+        
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
